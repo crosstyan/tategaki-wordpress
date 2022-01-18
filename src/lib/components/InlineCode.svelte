2c6ab7ae27b67f8e9d4c09ab92ca3e35a87f6896
Crosstyan <cf595833532@gmail.com>
2022-01-18 15:10:37 +0800

    feat: inline code tooltip

--- src/lib/components/InlineCode.svelte
+++ src/lib/components/InlineCode.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+  import Tooltip from "./Tooltip.svelte"
   export let codeElem: HTMLElement
-  // TODO: inlineCode hover to display horizontal code
   // https://www.npmjs.com/package/svelte-tooltip
   // https://svelte.dev/repl/dd6754a2ad0547c5b1c1ea37c0293fef?version=3.46.2
   // Tooltip
+  // TODO: maybe it's better to use `use:tooltip` instead of component
 </script>
 
-<!-- markup (zero or more items) goes here -->
-<span
-  class="code text-gray-300 text-sm font-mono my-1 py-2 bg-slate-600 no-underline"
-  >{@html codeElem.outerHTML}</span
->
+<Tooltip title={codeElem.innerText}>
+  <span
+    class="code text-gray-300 text-sm font-mono my-1 py-2 bg-slate-600 no-underline"
+    >{@html codeElem.outerHTML}</span
+  >
+</Tooltip>
 
 <style lang="postcss">
   /* your styles go here */
---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  // https://svelte.dev/repl/dd6754a2ad0547c5b1c1ea37c0293fef?version=3.46.2
+	export let title;
+	let isHovered = false;
+	let x;
+	let y;
+	
+	function mouseOver(event) {
+		isHovered = true;
+		x = event.pageX + 5;
+		y = event.pageY + 5;
+	}
+	function mouseMove(event) {
+		x = event.pageX + 5;
+		y = event.pageY + 5;
+	}
+	function mouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<span
+	on:mouseover={mouseOver}
+  on:mouseleave={mouseLeave}
+	on:mousemove={mouseMove}>
+	<slot />
+</span>
+
+{#if isHovered}
+	<div style="top: {y}px; left: {x}px; writing-mode: horizontal-tb;" class="tooltip">{title}</div>
+{/if}
+
+<style>
+	.tooltip {
+    display: inline-block;
+		border: 1px solid #ddd;
+		box-shadow: 1px 1px 1px #ddd;
+		background: white;
+		border-radius: 4px;
+		padding: 4px;
+		position: absolute;
+	}
+</style>
