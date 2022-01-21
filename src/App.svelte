4a2b63fe4f234c024390e02bee84009cc39404d1
Crosstyan <cf595833532@gmail.com>
2022-01-22 04:47:54 +0800

    fix: Safari dropdown bug

--- src/App.svelte
+++ src/App.svelte
@@ -45,8 +45,8 @@
         >
       </div>
     </Link>
-    <div class="dropdown dropdown-top dropdown-left" style="writing-mode: horizontal-tb;">
-      <div class="flex-none">
+    <div class="dropdown dropdown-top dropdown-end" style="writing-mode: horizontal-tb;">
+      <div tabindex="0" class="flex-none">
         <button class="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">      
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>                    
