eac4173b25a5fa5429664039b216e17c8665da1e
Crosstyan <cf595833532@gmail.com>
2022-01-25 15:46:12 +0800

    feat: alert in iOS

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -64,9 +64,16 @@
     <Breadcrumbs {pathname} />
   </div>
   {#if iOS()}
-    <div class="my-16 mx-4 md:mr-8 md:my-8">
-      <p class="font-serif">致 iOS 用户：由于浏览器的限制，Safari 浏览器无法正确渲染本网页，若遇到代码或者表格无法滚动或标点渲染异常，亦或是按钮渲染错误，请勿惊慌，此乃已知问题。</p>
-      <p class="font-serif text-sm">若欲查看源代码请点击 View Source 按钮。</p>
+    <div class="alert my-16 mx-4 md:mr-8 md:my-8 flex-row">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#2196f3" class="w-6 h-6 mx-2">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>                          
+        </svg> 
+      </div>
+      <div class="flex-2 flex-col">
+        <p class="font-serif">致 iOS 用户：由于浏览器的限制，Safari 浏览器无法正确渲染本网页，若遇到代码或者表格无法滚动或标点渲染异常，亦或是按钮渲染错误，请勿惊慌，此乃已知问题。</p>
+        <span class="font-serif text-sm">若欲查看源代码请点击 View Source 按钮。</span>
+      </div>
     </div>
   {/if}
 </header>
